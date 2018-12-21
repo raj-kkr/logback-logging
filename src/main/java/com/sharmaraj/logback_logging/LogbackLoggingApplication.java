dd82c5f35441e913a99e660a250f7e235aca5a8e
rajunify <raj.sharma@oyorooms.com>
2018-12-21 09:39:02 +0530

    first commit

---
+++ src/main/java/com/sharmaraj/logback_logging/LogbackLoggingApplication.java
@@ -0,0 +1,14 @@
+package com.sharmaraj.logback_logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogbackLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogbackLoggingApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/sharmaraj/logback_logging/LogbackLoggingApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sharmaraj.logback_logging;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LogbackLoggingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
